425b4ee97bdb8e57e24b10769051c9c8ac8eb1f3
genio1973 <plinio.sacchetti@heig-vd.ch>
2017-08-18 23:41:05 +0200

    No more email by default in login form. It was used to be faster to log on.

--- angular-project/angular2-http/src/app/public/login/login.component.ts
+++ angular-project/angular2-http/src/app/public/login/login.component.ts
@@ -10,7 +10,7 @@ import { Router } from "@angular/router";
 
 export class LoginComponent implements OnInit {
 
-  credentials = { email:'heinz@gmail.com', password: ''};
+  credentials = { email:'', password: ''};
   successMessage: string = '';
   errorMessage: string = '';
 
